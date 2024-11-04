14568a51547d0a92b52e95bf64a6f5557a4faabc
leexzu1 <jackie20029655@gmail.com>
2024-11-04 20:21:49 +0900

    [feat]AllServices

--- line4thon_site/src/pages/Login.styled.js
+++ line4thon_site/src/pages/Login.styled.js
@@ -1,16 +1,16 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 // Wrapper 스타일 정의
 export const Wrapper = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: calc(100% - 200px);
-    height: calc(100% - 120px);
-    background-color: #e9ecef;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: calc(100% - 200px);
+  height: calc(100% - 120px);
+  background-color: #e9ecef;
 `;
 
 // Menubar 스타일 정의
 export const MenuBar = styled.div`
-    font-size: 1rem;
+  font-size: 1rem;
 `;
