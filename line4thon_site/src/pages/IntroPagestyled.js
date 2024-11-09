6fa19e1a2ce2ee144354c871baee65e362803140
Hyojun Jeon <hyojunj@naver.com>
2024-11-10 03:33:14 +0900

    [feat] 노선도 서비스 카드 구현

--- line4thon_site/src/pages/IntroPagestyled.js
+++ line4thon_site/src/pages/IntroPagestyled.js
@@ -1,6 +1,7 @@
 import styled, { css, keyframes } from 'styled-components';
 
 export const IntroPage = styled.div`
+    position: relative;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -33,7 +34,7 @@ export const FirstText = styled.div`
   justify-content: center;
   gap: 40px;
   opacity: 0;
-  animation: ${({firstTextVisible}) => firstTextVisible ? fadeIn : 'none'} 2s forwards;
+  animation: ${({ firstTextVisible }) => firstTextVisible ? fadeIn : 'none'} 2s forwards;
   scroll-snap-align: start;
 
 
@@ -55,6 +56,84 @@ export const FirstText = styled.div`
     line-height: normal;
   }
 `
+export const ServiceCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #404040;
+  transition: transform 0.1s ease-out;
+  overflow: hidden;
+  border-radius: ${({ scrollRatio }) => `${4 + scrollRatio * 3}px`};
+  position: relative;
+  position: absolute;
+  top: ${({ scrollRatio }) => `${75 + scrollRatio * 50}vh`}; // 50vh에서 100vh까지 이동
+  width: ${({ scrollRatio }) => `${30 * 5 + scrollRatio * 22 * 5}px`}; // 100px에서 200px로 확장
+  height: ${({ scrollRatio }) => `${30 * 4 + scrollRatio * 22 * 4}px`}; // 100px에서 200px로 확장
+  transform: ${({ scrollRatio }) => `scale(${1 + scrollRatio})`};
+  box-shadow: rgba(100, 100, 100, 0.2) 0px 2px 20px;
+ 
+  &:hover .overlay {
+    opacity: 1;
+  }
+
+  &:hover .view-more-btn {
+    opacity: 1;
+    transform: translateY(0);
+  }
+
+  img{
+    width: 100%;
+  }
+  #descripCont{
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    padding: 6px 10px;
+  }
+  .common-text {
+    color: #FFF;
+    font-family: Pretendard;
+    line-height: normal;
+  }
+  #title{
+    font-size: 12px;
+    font-weight: 700;
+  }
+  #intro{
+    font-size: 10px;
+    font-weight:400;
+  }
+  #teamNum{
+    font-size: 8px;
+    font-weight: 200;
+    color: #B9B9B9;
+  }
+
+`
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3); // 어두운 반투명 오버레이
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const GoReviewBtn = styled.button`
+  opacity: 0;
+  background-color: white;
+  color: black;
+  border: none;
+  padding: 10px 20px;
+  font-size: 1em;
+  cursor: pointer;
+  transition: opacity 0.3s ease, transform 0.3s ease;
+  transform: translateY(20px); // 초기 위치를 살짝 아래로
+`;
 
 export const NextIcon = styled.div`
   position: absolute;
