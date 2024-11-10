832c0b0cf65dcae87de43fdce89111ab331d9484
leexzu1 <jackie20029655@gmail.com>
2024-11-10 23:47:30 +0900

    [feat]Ranking

--- line4thon_site/src/pages/Ranking.styled.js
+++ line4thon_site/src/pages/Ranking.styled.js
@@ -1,22 +1,21 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    // position: absolute;
-    display: flex;
-    flex-direction: column;
-    top: 0;
-    right: 0;
-    width: calc(100% - 200px);
-    margin-left: 200px;
-    // height: calc(100% - 120px);
-    // width: 100%;
-    height: 100%;
-    background-color: #e9ecef;
+  // position: absolute;
+  display: flex;
+  flex-direction: column;
+  top: 0;
+  right: 0;
+  width: calc(100% - 200px);
+  margin-left: 200px;
+  // height: calc(100% - 120px);
+  // width: 100%;
+  height: 100%;
+  background-color: #e9ecef;
 `;
 
 // Menubar 스타일 정의
 export const MenuBar = styled.div`
-
   display: flex;
 `;
 
