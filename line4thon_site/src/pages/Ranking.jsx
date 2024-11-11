0cd37868c2c81b805da67e5cb8095d4b60968846
leexzu1 <jackie20029655@gmail.com>
2024-11-12 03:16:06 +0900

    [feat]Ranking

--- line4thon_site/src/pages/Ranking.jsx
+++ line4thon_site/src/pages/Ranking.jsx
@@ -1,62 +1,150 @@
 import React from "react";
 import * as Styled from "./Ranking.styled";
+import ArrowImage from "../assets/Arrowright.png";
+import RankingImage from "../assets/Ranking.png";
 
 const Ranking = () => {
-  // 각 부문과 서비스 목록은 추후 API 데이터로 대체할 예정입니다.
+  // 예시 데이터 - 나중에 API 데이터로 대체할 예정
+  const rankingServices = [
+    {
+      rank: 1,
+      image: "/path/to/image-1.png",
+      name: "[서비스명]",
+      description: "서비스 한줄소개",
+    },
+    {
+      rank: 2,
+      image: "/path/to/image-2.png",
+      name: "[너만 노란색]",
+    },
+    {
+      rank: 3,
+      image: "/path/to/image-3.png",
+      name: "[서브웨이]",
+    },
+    {
+      rank: 4,
+      image: "/path/to/image-4.png",
+      name: "[지하철 대탐험]",
+    },
+    {
+      rank: 5,
+      image: "/path/to/image-5.png",
+      name: "[서브웨이]",
+    },
+  ];
+
+  const badgeServices = [
+    {
+      rank: 1,
+      image: "/path/to/image-1.png",
+      name: "[서비스명]",
+      description: "서비스 한줄소개",
+    },
+    {
+      rank: 2,
+      image: "/path/to/image-2.png",
+      name: "[너만 노란색]",
+    },
+    {
+      rank: 3,
+      image: "/path/to/image-3.png",
+      name: "[서브웨이]",
+    },
+    {
+      rank: 4,
+      image: "/path/to/image-4.png",
+      name: "[지하철 대탐험]",
+    },
+    {
+      rank: 5,
+      image: "/path/to/image-5.png",
+      name: "[서브웨이]",
+    },
+  ];
+
+  // 각 부문의 1등과 나머지 항목들을 분리
+  const firstPlaceRanking = rankingServices[0];
+  const otherPlacesRanking = rankingServices.slice(1);
+
+  const firstPlaceBadge = badgeServices[0];
+  const otherPlacesBadge = badgeServices.slice(1);
+
   return (
     <Styled.Wrapper>
       <Styled.Content>
         <Styled.Header>
           <Styled.Title>명예의 전당</Styled.Title>
-          <Styled.Subtitle>최고의 서비스를 랭킹별로 만나보세요</Styled.Subtitle>
+          <Styled.Subtitle>최고의 서비스를 위해 참여해주세요</Styled.Subtitle>
         </Styled.Header>
 
-        <Styled.Section>
-          <Styled.SectionTitle>총 별점 부문</Styled.SectionTitle>
-          <Styled.ServiceList>
-            {[1, 2, 3, 4, 5].map((rank) => (
-              <Styled.ServiceItem key={rank}>
-                <Styled.Rank>{rank}</Styled.Rank>
-                <Styled.ServiceImage src={`/path/to/image-${rank}.png`} alt={`Service ${rank}`} />
-                <Styled.ServiceInfo>
-                  <Styled.ServiceName>서비스 {rank}</Styled.ServiceName>
-                  <Styled.ServiceDetails>상세 정보</Styled.ServiceDetails>
-                </Styled.ServiceInfo>
-              </Styled.ServiceItem>
-            ))}
-          </Styled.ServiceList>
-        </Styled.Section>
+        <Styled.SectionContainer>
+          {/* 총 별점 부문 */}
+          <Styled.Section>
+            <Styled.SectionTitleAll>
+              <img src={RankingImage} alt="RankingImage" width="35" height="35" />
+              <Styled.SectionTitle>총 별점 부문</Styled.SectionTitle>
+            </Styled.SectionTitleAll>
 
-        <Styled.Section>
-          <Styled.SectionTitle>뱃지 부문</Styled.SectionTitle>
-          <Styled.ServiceList>
-            {[1, 2, 3, 4, 5].map((rank) => (
-              <Styled.ServiceItem key={rank}>
-                <Styled.Rank>{rank}</Styled.Rank>
-                <Styled.ServiceImage src={`/path/to/image-${rank}.png`} alt={`Service ${rank}`} />
-                <Styled.ServiceInfo>
-                  <Styled.ServiceName>서비스 {rank}</Styled.ServiceName>
-                  <Styled.ServiceDetails>상세 정보</Styled.ServiceDetails>
-                </Styled.ServiceInfo>
-              </Styled.ServiceItem>
-            ))}
-          </Styled.ServiceList>
-        </Styled.Section>
+            {/* 1등 항목 */}
+            <Styled.FirstPlace>
+              <Styled.FirstPlaceImage src={firstPlaceRanking.image} alt={`Service ${firstPlaceRanking.rank}`} />
+              <Styled.FirstPlaceInfo>
+                <Styled.ServiceName>{firstPlaceRanking.name}</Styled.ServiceName>
+                <Styled.ServiceDescription>{firstPlaceRanking.description}</Styled.ServiceDescription>
+              </Styled.FirstPlaceInfo>
+            </Styled.FirstPlace>
 
-        <Styled.AllServicesSection>
-          <Styled.SectionTitle>'4호선 톤'의 모든 서비스들</Styled.SectionTitle>
-          <Styled.ServiceList>
-            {[1, 2, 3, 4, 5].map((rank) => (
-              <Styled.ServiceItem key={rank}>
-                <Styled.ServiceImage src={`/path/to/image-${rank}.png`} alt={`Service ${rank}`} />
-                <Styled.ServiceInfo>
-                  <Styled.ServiceName>서비스 {rank}</Styled.ServiceName>
-                  <Styled.ServiceDetails>상세 정보</Styled.ServiceDetails>
-                </Styled.ServiceInfo>
-              </Styled.ServiceItem>
-            ))}
-          </Styled.ServiceList>
-        </Styled.AllServicesSection>
+            {/* 2등부터 나머지 항목들 */}
+            <Styled.ServiceList>
+              {otherPlacesRanking.map((service) => (
+                <Styled.ServiceItem key={service.rank}>
+                  <Styled.Rank>{service.rank}</Styled.Rank>
+                  <Styled.ServiceImage src={service.image} alt={`Service ${service.rank}`} />
+                  <Styled.ServiceInfo>
+                    <Styled.ServiceName>{service.name}</Styled.ServiceName>
+                  </Styled.ServiceInfo>
+                  <Styled.Arrow>
+                    <img src={ArrowImage} alt="Arrow Right" width="24" height="24" />
+                  </Styled.Arrow>
+                </Styled.ServiceItem>
+              ))}
+            </Styled.ServiceList>
+          </Styled.Section>
+
+          {/* 뱃지 부문 */}
+          <Styled.Section>
+            <Styled.SectionTitleAll>
+              <img src={RankingImage} alt="RankingImage" width="35" height="35" />
+              <Styled.SectionTitle>뱃지 부문</Styled.SectionTitle>
+            </Styled.SectionTitleAll>
+
+            {/* 1등 항목 */}
+            <Styled.FirstPlace>
+              <Styled.FirstPlaceImage src={firstPlaceBadge.image} alt={`Service ${firstPlaceBadge.rank}`} />
+              <Styled.FirstPlaceInfo>
+                <Styled.ServiceName>{firstPlaceBadge.name}</Styled.ServiceName>
+                <Styled.ServiceDescription>{firstPlaceBadge.description}</Styled.ServiceDescription>
+              </Styled.FirstPlaceInfo>
+            </Styled.FirstPlace>
+
+            {/* 2등부터 나머지 항목들 */}
+            <Styled.ServiceList>
+              {otherPlacesBadge.map((service) => (
+                <Styled.ServiceItem key={service.rank}>
+                  <Styled.Rank>{service.rank}</Styled.Rank>
+                  <Styled.ServiceImage src={service.image} alt={`Service ${service.rank}`} />
+                  <Styled.ServiceInfo>
+                    <Styled.ServiceName>{service.name}</Styled.ServiceName>
+                  </Styled.ServiceInfo>
+                  <Styled.Arrow>
+                    <img src={ArrowImage} alt="Arrow Right" width="24" height="24" />
+                  </Styled.Arrow>
+                </Styled.ServiceItem>
+              ))}
+            </Styled.ServiceList>
+          </Styled.Section>
+        </Styled.SectionContainer>
       </Styled.Content>
     </Styled.Wrapper>
   );
