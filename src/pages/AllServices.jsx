dc11898a772093ceecec53c28d7874beeffcb7a5
leexzu1 <jackie20029655@gmail.com>
2024-11-14 11:43:11 +0900

    [feat]Ranking 진행중

--- src/pages/AllServices.jsx
+++ src/pages/AllServices.jsx
@@ -24,8 +24,8 @@ const AllServices = () => {
   }, []);
 
   const GoDetail = (service) => {
-    navigate(`/Detail/${service.id}`)
-  }
+    navigate(`/Detail/${service.id}`);
+  };
 
   return (
     <Styled.Wrapper>
@@ -49,7 +49,7 @@ const AllServices = () => {
       {/* 서비스 카드 그리드 */}
       <Styled.CardGrid>
         {services.map((service) => (
-          <Styled.ServiceCardAll key={service.id}>
+          <Styled.ServiceCardAll key={service.id} onClick={() => GoDetail(service)}>
             <Styled.ServiceCard>
               <Styled.CardImage src={service.thumbnail_image} alt={`${service.service_name} 이미지`} />
             </Styled.ServiceCard>
@@ -59,45 +59,8 @@ const AllServices = () => {
           </Styled.ServiceCardAll>
         ))}
       </Styled.CardGrid>
-    <>
-      <Header
-        isWhiteBackground={true}
-      />
-      <Styled.Wrapper>
-        <Styled.Title>
-          <Styled.TitleTop>
-            <span className="singlequo">'</span>
-            <span className="smalltitle1">4호선톤</span>
-            <span className="singlequo">'</span>
-          </Styled.TitleTop>
-
-          <span className="smalltitle2">모든 서비스</span>
-          <p className="subtitle">최고의 서비스를 위해 리뷰해주세요</p>
-        </Styled.Title>
-
-        <Styled.SearchBar>
-          <img src={Search} alt="검색 아이콘" className="search-icon" />
-          <input type="text" placeholder="서비스 명으로 검색해보세요" />
-        </Styled.SearchBar>
-
-        {/* 서비스 카드 그리드 */}
-        <Styled.CardGrid>
-          {services.map((service) => (
-            <Styled.ServiceCardAll key={service.id} onClick={() => GoDetail(service)}>
-              <Styled.ServiceCard>
-                <Styled.CardImage src={service.thumbnail_image} alt={`${service.service_name} 이미지`} />
-              </Styled.ServiceCard>
-              <Styled.CardText>
-                <span className="service-name">[{service.service_name}] </span> by 팀 {service.team}
-              </Styled.CardText>
-            </Styled.ServiceCardAll>
-          ))}
-        </Styled.CardGrid>
-      </Styled.Wrapper>
       <Footer />
     </Styled.Wrapper>
-
-    </>
   );
 };
 
--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,86 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as Styled from "./Ranking.styled";
 import ArrowImage from "../assets/Arrowright.png";
 import RankingImage from "../assets/Ranking.png";
 
-const Ranking = () => {
-  const rankingServices = [
-    {
-      rank: 1,
-      rankChange: 0,
-      image: "/path/to/image-1.png",
-      name: "[서비스명]",
-      description: "서비스 한줄소개",
-    },
-    {
-      rank: 2,
-      rankChange: -4,
-      image: "/path/to/image-2.png",
-      name: "[너만 노란색]",
-    },
-    {
-      rank: 3,
-      rankChange: 4,
-      image: "/path/to/image-3.png",
-      name: "[서브웨이]",
-    },
-    {
-      rank: 4,
-      rankChange: 0,
-      image: "/path/to/image-4.png",
-      name: "[지하철 대탐험]",
-    },
-    {
-      rank: 5,
-      rankChange: 0,
-      image: "/path/to/image-5.png",
-      name: "[서브웨이]",
-    },
-  ];
+const Ranking = ({ API_BASE_URL }) => {
+  const [rankingServices, setRankingServices] = useState([]);
+  const [badgeServices, setBadgeServices] = useState([]);
+  const token = localStorage.getItem("accessToken");
 
-  const badgeServices = [
-    {
-      rank: 1,
-      image: "/path/to/image-1.png",
-      name: "[서비스명]",
-      description: "서비스 한줄소개",
-    },
-    {
-      rank: 2,
-      image: "/path/to/image-2.png",
-      name: "[너만 노란색]",
-    },
-    {
-      rank: 3,
-      image: "/path/to/image-3.png",
-      name: "[서브웨이]",
-    },
-    {
-      rank: 4,
-      image: "/path/to/image-4.png",
-      name: "[지하철 대탐험]",
-    },
-    {
-      rank: 5,
-      image: "/path/to/image-5.png",
-      name: "[서브웨이]",
-    },
-  ];
+  const totalScoreApiUrl = `${API_BASE_URL}/main/HOF-score`;
+  const badgeApiUrl = `${API_BASE_URL}/main/HOF_badge`;
 
-  const firstPlaceRanking = rankingServices[0];
-  const otherPlacesRanking = rankingServices.slice(1);
+  useEffect(() => {
+    const fetchTotalScoreData = async () => {
+      try {
+        const response = await fetch(totalScoreApiUrl, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
 
-  const firstPlaceBadge = badgeServices[0];
-  const otherPlacesBadge = badgeServices.slice(1);
+        const data = await response.json();
+        setRankingServices(data);
+      } catch (error) {
+        console.error("총 별점 데이터를 가져오는 중 오류 발생:", error);
+      }
+    };
+
+    const fetchBadgeData = async () => {
+      try {
+        const response = await fetch(badgeApiUrl, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+
+        const data = await response.json();
+        setBadgeServices(data);
+      } catch (error) {
+        console.error("뱃지 데이터를 가져오는 중 오류 발생:", error);
+      }
+    };
+
+    // 두 가지 데이터를 모두 호출하도록 수정
+    fetchTotalScoreData();
+    fetchBadgeData();
+  }, [token, totalScoreApiUrl, badgeApiUrl]);
 
   return (
     <Styled.Wrapper>
       <Styled.Content>
         <Styled.Header>
-          <Styled.TitleWrapper>
-            <Styled.Title>명예의 전당</Styled.Title>
-            <Styled.RankingImageStyled src={RankingImage} alt="RankingImage" />
-          </Styled.TitleWrapper>
+          <Styled.Title>명예의 전당</Styled.Title>
           <Styled.Subtitle>최고의 서비스를 위해 참여해주세요</Styled.Subtitle>
         </Styled.Header>
 
@@ -93,29 +68,26 @@ const Ranking = () => {
             </Styled.SectionTitleAll>
 
             {/* 1등 항목 */}
-            <Styled.FirstPlace>
-              <Styled.FirstPlaceImage src={firstPlaceRanking.image} alt={`Service ${firstPlaceRanking.rank}`} />
-              <Styled.FirstPlaceInfo>
-                <Styled.ServiceNameFirst>{firstPlaceRanking.name}</Styled.ServiceNameFirst>
-                <Styled.ServiceDescriptionFirst>{firstPlaceRanking.description}</Styled.ServiceDescriptionFirst>
-              </Styled.FirstPlaceInfo>
-            </Styled.FirstPlace>
+            {rankingServices.length > 0 && (
+              <Styled.FirstPlace as="a" href={rankingServices[0].site_url} target="_blank" rel="noopener noreferrer">
+                <Styled.FirstPlaceImage src={rankingServices[0].thumbnail_image} alt={`Service ${rankingServices[0].id}`} />
+                <Styled.FirstPlaceInfo>
+                  <Styled.ServiceNameFirst>{rankingServices[0].service_name}</Styled.ServiceNameFirst>
+                  <Styled.ServiceDescriptionFirst>{rankingServices[0].team_name}</Styled.ServiceDescriptionFirst>
+                </Styled.FirstPlaceInfo>
+              </Styled.FirstPlace>
+            )}
 
             {/* 2등부터 나머지 항목들 */}
             <Styled.ServiceList>
-              {otherPlacesRanking.map((service) => (
-                <Styled.ServiceItem key={service.rank}>
-                  <Styled.Rank>
-                    {service.rank}
-                    {service.rankChange > 0 && <Styled.RankChangeUp>▲{service.rankChange}</Styled.RankChangeUp>}
-                    {service.rankChange < 0 && <Styled.RankChangeDown>▼{Math.abs(service.rankChange)}</Styled.RankChangeDown>}
-                    {service.rankChange === 0 && <Styled.RankChangeNone>-</Styled.RankChangeNone>}
-                  </Styled.Rank>
-                  <Styled.ServiceImage src={service.image} alt={`Service ${service.rank}`} />
+              {rankingServices.slice(1).map((service) => (
+                <Styled.ServiceItem key={service.id}>
+                  <Styled.Rank>{service.total_score}</Styled.Rank>
+                  <Styled.ServiceImage src={service.thumbnail_image} alt={`Service ${service.id}`} />
                   <Styled.ServiceInfo>
-                    <Styled.ServiceName>{service.name}</Styled.ServiceName>
+                    <Styled.ServiceName>{service.service_name}</Styled.ServiceName>
                   </Styled.ServiceInfo>
-                  <Styled.Arrow>
+                  <Styled.Arrow as="a" href={service.site_url} target="_blank" rel="noopener noreferrer">
                     <img src={ArrowImage} alt="Arrow Right" width="24" height="24" />
                   </Styled.Arrow>
                 </Styled.ServiceItem>
@@ -131,29 +103,26 @@ const Ranking = () => {
             </Styled.SectionTitleAll>
 
             {/* 1등 항목 */}
-            <Styled.FirstPlace>
-              <Styled.FirstPlaceImage src={firstPlaceBadge.image} alt={`Service ${firstPlaceBadge.rank}`} />
-              <Styled.FirstPlaceInfo>
-                <Styled.ServiceNameFirst>{firstPlaceBadge.name}</Styled.ServiceNameFirst>
-                <Styled.ServiceDescriptionFirst>{firstPlaceBadge.description}</Styled.ServiceDescriptionFirst>
-              </Styled.FirstPlaceInfo>
-            </Styled.FirstPlace>
+            {badgeServices.length > 0 && (
+              <Styled.FirstPlace as="a" href={badgeServices[0].site_url} target="_blank" rel="noopener noreferrer">
+                <Styled.FirstPlaceImage src={badgeServices[0].thumbnail_image} alt={`Service ${badgeServices[0].id}`} />
+                <Styled.FirstPlaceInfo>
+                  <Styled.ServiceNameFirst>{badgeServices[0].service_name}</Styled.ServiceNameFirst>
+                  <Styled.ServiceDescriptionFirst>{badgeServices[0].team_name}</Styled.ServiceDescriptionFirst>
+                </Styled.FirstPlaceInfo>
+              </Styled.FirstPlace>
+            )}
 
             {/* 2등부터 나머지 항목들 */}
             <Styled.ServiceList>
-              {otherPlacesRanking.map((service) => (
-                <Styled.ServiceItem key={service.rank}>
-                  <Styled.Rank>
-                    {service.rank}
-                    {service.rankChange > 0 && <Styled.RankChangeUp>▲{service.rankChange}</Styled.RankChangeUp>}
-                    {service.rankChange < 0 && <Styled.RankChangeDown>▼{Math.abs(service.rankChange)}</Styled.RankChangeDown>}
-                    {service.rankChange === 0 && <Styled.RankChangeNone>-</Styled.RankChangeNone>}
-                  </Styled.Rank>
-                  <Styled.ServiceImage src={service.image} alt={`Service ${service.rank}`} />
+              {badgeServices.slice(1).map((service) => (
+                <Styled.ServiceItem key={service.id}>
+                  <Styled.Rank>{service.tag_num}</Styled.Rank>
+                  <Styled.ServiceImage src={service.thumbnail_image} alt={`Service ${service.id}`} />
                   <Styled.ServiceInfo>
-                    <Styled.ServiceName>{service.name}</Styled.ServiceName>
+                    <Styled.ServiceName>{service.service_name}</Styled.ServiceName>
                   </Styled.ServiceInfo>
-                  <Styled.Arrow>
+                  <Styled.Arrow as="a" href={service.site_url} target="_blank" rel="noopener noreferrer">
                     <img src={ArrowImage} alt="Arrow Right" width="24" height="24" />
                   </Styled.Arrow>
                 </Styled.ServiceItem>
