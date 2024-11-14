e8d1b8d0276440193e308645ed84dfab1e284de6
leexzu1 <jackie20029655@gmail.com>
2024-11-15 02:41:55 +0900

    [feat]AuthContext.jsx 오타수정

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
@@ -9,7 +9,7 @@ export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const token = localStorage.getItem('accessToken');
+    const token = localStorage.getItem("accessToken");
     if (token) {
       if (isTokenExpired(token)) {
         // 토큰이 만료되었을 때 자동 로그아웃
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }) => {
   const isTokenExpired = (token) => {
     // JWT 토큰이 base64로 인코딩되었을 경우 디코딩하여 만료 시간을 확인
     try {
-      const payload = JSON.parse(atob(token.split('.')[1]));
+      const payload = JSON.parse(atob(token.split(".")[1]));
       const expiry = payload.exp * 1000; // 만료 시간을 밀리초로 변환
       return Date.now() > expiry;
     } catch (error) {
@@ -34,24 +34,20 @@ export const AuthProvider = ({ children }) => {
   };
 
   const login = (token) => {
-    localStorage.setItem('accessToken', token);
+    localStorage.setItem("accessToken", token);
     setAccessToken(token);
     setIsAuthenticated(true);
-    navigate('/'); // 로그인 후 이동할 경로
+    navigate("/"); // 로그인 후 이동할 경로
   };
 
   const logout = () => {
-    localStorage.removeItem('accessToken');
+    localStorage.removeItem("accessToken");
     setAccessToken(null);
     setIsAuthenticated(false);
-    navigate('/login'); // 로그아웃 후 로그인 페이지로 이동
+    navigate("/login"); // 로그아웃 후 로그인 페이지로 이동
   };
 
-  return (
-    <AuthContext.Provider value={{ setIsAuthenticateds, isAuthenticated, accessToken, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={{ setIsAuthenticated, isAuthenticated, accessToken, login, logout }}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = () => useContext(AuthContext);
